6137b82ebc6317ff3d8af7d3c3ff5c90ff63b002
山﨑 亜寿紗 <az-yamasaki@virtualex.local>
2019-04-26 11:14:35 +0900

    #2 create Expense.java

--- src/main/java/java_s04/ExpenseResource.java
+++ src/main/java/java_s04/ExpenseResource.java
@@ -1,5 +1,22 @@
 package java_s04;
 
-public class ExpenseResource {
+import java.util.List;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+import dao.ExpenseDAO;
+
+public class ExpenseResource {
+	private final ExpenseDAO dao = new ExpenseDAO();
+
+	/**
+	 * 一覧用に経費情報を全件取得する。
+	 * @return 経費情報のリストをJSON形式で返す。
+	 */
+	@GET
+	@Produces()
+	public List<Expense> findAll(){
+		return dao.findAll();
+	}
 }
